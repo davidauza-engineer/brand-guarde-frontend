d85c2256b0b6e6047f63b07e6344caf8cbbc9304
davidauza.engineer <davidauza01@gmail.com>
2020-05-29 14:50:16 -0500

    Implement add chain component

--- src/app/components/chain/add-chain/add-chain.component.ts
+++ src/app/components/chain/add-chain/add-chain.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChainService } from 'src/app/services/chain/chain.service';
 
 @Component({
   selector: 'app-add-chain',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-chain.component.css']
 })
 export class AddChainComponent implements OnInit {
+ chain = {
+    name: ''
+  };
+  submitted = false;
+  errors = '';
 
-  constructor() { }
+  constructor(private chainService: ChainService) { }
 
   ngOnInit() {
   }
 
+  saveChain() {
+    const data = {
+      chain: {
+        name: this.chain.name
+      }
+    };
+
+    this.chainService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.errors = ''
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  newChain() {
+    this.submitted = false;
+    this.errors = ''
+    this.chain = {
+      name: ''
+    };
+  }
 }
