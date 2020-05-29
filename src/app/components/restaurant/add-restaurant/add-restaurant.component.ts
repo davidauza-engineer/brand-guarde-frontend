5fb2368403b0c87c7a40165ef20a39c3e00b103d
davidauza.engineer <davidauza01@gmail.com>
2020-05-29 14:37:03 -0500

    Implement add restaurant component

--- src/app/components/restaurant/add-restaurant/add-restaurant.component.ts
+++ src/app/components/restaurant/add-restaurant/add-restaurant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from 'src/app/services/restaurant/restaurant.service';
 
 @Component({
   selector: 'app-add-restaurant',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-restaurant.component.css']
 })
 export class AddRestaurantComponent implements OnInit {
+  restaurant = {
+    chain_id: '',
+    country_id: '',
+    name: '',
+    address: ''
+  };
+  submitted = false;
+  errors = '';
 
-  constructor() { }
+  constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit() {
   }
 
+  saveRestaurant() {
+    const data = {
+      restaurant: {
+        chain_id: this.restaurant.chain_id,
+        country_id: this.restaurant.country_id,
+        name: this.restaurant.name,
+        address: this.restaurant.address
+      }
+    };
+
+    this.restaurantService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.errors = ''
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  newRestaurant() {
+    this.submitted = false;
+    this.errors = ''
+    this.restaurant = {
+      chain_id: '',
+      country_id: '',
+      name: '',
+      address: ''
+    };
+  }
 }
