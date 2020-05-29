acceae5417db06cb9dc3fca688073e80a8e8f435
davidauza.engineer <davidauza01@gmail.com>
2020-05-29 15:31:29 -0500

    Implement users list component

--- src/app/components/user/users-list/users-list.component.ts
+++ src/app/components/user/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+  users: any;
+  currentUser = null;
+  currentIndex = -1;
+  errors = ''
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.retrieveUsers();
   }
 
+  retrieveUsers() {
+    this.userService.getAll()
+      .subscribe(
+        data => {
+          this.users = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error)
+        });
+  }
+
+  refreshList() {
+    this.retrieveUsers();
+    this.currentUser = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveUser(user, index) {
+    this.currentUser = user;
+    this.currentIndex = index;
+  }
 }
