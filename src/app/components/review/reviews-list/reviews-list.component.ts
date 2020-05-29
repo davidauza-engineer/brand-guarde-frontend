a1c4ee78de15716aba6412e927c5aa6a0c617f57
davidauza.engineer <davidauza01@gmail.com>
2020-05-29 15:48:00 -0500

    Implement reviews list component

--- src/app/components/review/reviews-list/reviews-list.component.ts
+++ src/app/components/review/reviews-list/reviews-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReviewService } from 'src/app/services/review/review.service';
 
 @Component({
   selector: 'app-reviews-list',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsListComponent implements OnInit {
 
-  constructor() { }
+  reviews: any;
+  currentReview = null;
+  currentIndex = -1;
+  errors = ''
+
+  constructor(private reviewService: ReviewService) { }
 
   ngOnInit() {
+    this.retrieveReviews();
   }
 
+  retrieveReviews() {
+    this.reviewService.getAll()
+      .subscribe(
+        data => {
+          this.reviews = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error)
+        });
+  }
+
+  refreshList() {
+    this.retrieveReviews();
+    this.currentReview = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveReview(review, index) {
+    this.currentReview = review;
+    this.currentIndex = index;
+  }
 }
