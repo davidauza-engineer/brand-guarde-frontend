5482165e0c70284a417257e54fb4abdbab4ea90f
davidauza.engineer <davidauza01@gmail.com>
2020-05-29 11:58:25 -0500

    WIP: Implement add user component

--- src/app/components/user/add-user/add-user.component.ts
+++ src/app/components/user/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-add-user',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
+  user = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+  };
+  submitted = false;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  saveUser() {
+    const data = {
+      name: this.user.name,
+      email: this.user.email,
+      password: this.user.password,
+      password_confirmation: this.user.password_confirmation
+    };
+
+    this.userService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newUser() {
+    this.submitted = false;
+    this.user = {
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: ''
+    };
+  }
 }
