e7b760891889ff4bca06b5f440ca98a2fc8e4626
davidauza.engineer <davidauza01@gmail.com>
2020-05-29 16:25:20 -0500

    Implement chains list component

--- src/app/components/chain/chains-list/chains-list.component.ts
+++ src/app/components/chain/chains-list/chains-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChainService } from 'src/app/services/chain/chain.service';
 
 @Component({
   selector: 'app-chains-list',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChainsListComponent implements OnInit {
 
-  constructor() { }
+  chains: any;
+  currentChain = null;
+  currentIndex = -1;
+  errors = ''
+
+  constructor(private chainService: ChainService) { }
 
   ngOnInit() {
+    this.retrieveChains();
   }
 
+  retrieveChains() {
+    this.chainService.getAll()
+      .subscribe(
+        data => {
+          this.chains = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error)
+        });
+  }
+
+  refreshList() {
+    this.retrieveChains();
+    this.currentChain = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveChain(chain, index) {
+    this.currentChain = chain;
+    this.currentIndex = index;
+  }
 }
