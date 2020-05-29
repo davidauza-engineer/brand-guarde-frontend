e8bac04294152abb325037ad22b5d8314a37ea03
davidauza.engineer <davidauza01@gmail.com>
2020-05-29 15:05:46 -0500

    Implement add country component

--- src/app/components/country/add-country/add-country.component.ts
+++ src/app/components/country/add-country/add-country.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryService } from 'src/app/services/country/country.service';
 
 @Component({
   selector: 'app-add-country',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-country.component.css']
 })
 export class AddCountryComponent implements OnInit {
+  country = {
+    name: ''
+  };
+  submitted = false;
+  errors = '';
 
-  constructor() { }
+  constructor(private countryService: CountryService) { }
 
   ngOnInit() {
   }
 
+  saveCountry() {
+    const data = {
+      country: {
+        name: this.country.name
+      }
+    };
+
+    this.countryService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.errors = ''
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  newCountry() {
+    this.submitted = false;
+    this.errors = ''
+    this.country = {
+      name: ''
+    };
+  }
 }
