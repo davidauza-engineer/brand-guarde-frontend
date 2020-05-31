98535e3bc68c122535c778f4b088c4068bb89a5d
davidauza.engineer <davidauza01@gmail.com>
2020-05-31 18:20:11 -0500

    Implement user details component

--- src/app/components/user/user-details/user-details.component.ts
+++ src/app/components/user/user-details/user-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
+  currentUser = null;
+  message = '';
+  errors = '';
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.message = '';
+    this.getUser(this.route.snapshot.paramMap.get('id'));
   }
 
+  getUser(id) {
+    this.userService.get(id)
+      .subscribe(
+        data => {
+          this.currentUser = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  updateUser() {
+    this.userService.update(this.currentUser.id, this.currentUser)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'The user was updated successfully!';
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  deleteUser() {
+    this.userService.delete(this.currentUser.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/users'])
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
 }
