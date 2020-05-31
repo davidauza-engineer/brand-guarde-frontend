68ecf45c5a0eb1d5f9ec2cdd70af7a7d1783816c
davidauza.engineer <davidauza01@gmail.com>
2020-05-31 18:36:19 -0500

    Implement review details component

--- src/app/components/review/review-details/review-details.component.ts
+++ src/app/components/review/review-details/review-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ReviewService } from 'src/app/services/review/review.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-review-details',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review-details.component.css']
 })
 export class ReviewDetailsComponent implements OnInit {
+  currentReview = null;
+  message = '';
+  errors = '';
 
-  constructor() { }
+  constructor(
+    private reviewService: ReviewService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.message = '';
+    this.getReview(this.route.snapshot.paramMap.get('id'));
   }
 
+  getReview(id) {
+    this.reviewService.get(id)
+      .subscribe(
+        data => {
+          this.currentReview = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  updateReview() {
+    this.reviewService.update(this.currentReview.id, this.currentReview)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'The review was updated successfully!';
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  deleteReview() {
+    this.reviewService.delete(this.currentReview.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/reviews'])
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
 }
