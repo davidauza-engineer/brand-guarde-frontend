192fdc10d4b8de516f22b4f48f7ed9886e2f2ad7
davidauza.engineer <davidauza01@gmail.com>
2020-05-31 19:16:04 -0500

    Implement restaurant details component

--- src/app/components/restaurant/restaurant-details/restaurant-details.component.ts
+++ src/app/components/restaurant/restaurant-details/restaurant-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from 'src/app/services/restaurant/restaurant.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-restaurant-details',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant-details.component.css']
 })
 export class RestaurantDetailsComponent implements OnInit {
+  currentRestaurant = null;
+  message = '';
+  errors = '';
 
-  constructor() { }
+  constructor(
+    private restaurantService: RestaurantService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.message = '';
+    this.getRestaurant(this.route.snapshot.paramMap.get('id'));
   }
 
+  getRestaurant(id) {
+    this.restaurantService.get(id)
+      .subscribe(
+        data => {
+          this.currentRestaurant = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  updateRestaurant() {
+    this.restaurantService.update(this.currentRestaurant.id, this.currentRestaurant)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'The restaurant was updated successfully!';
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
+
+  deleteRestaurant() {
+    this.restaurantService.delete(this.currentRestaurant.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/restaurants'])
+        },
+        error => {
+          console.log(error);
+          this.errors = JSON.stringify(error.error);
+        });
+  }
 }
